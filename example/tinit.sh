e00398ab9e91b4b812e6c1948c742a6e9bfefe1a
Steve Fibich <steve.fibich@nowhere.com>
2021-10-10 16:35:51 -0400

    removing files

--- example/tinit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-function test() {
-
-	terraform init --backend-config='storage_account_name=x63249403618806terraform' --backend-config='key=terraform.tfstate.dev.example-rg' --backend-config='container_name=terraform-state' --reconfigure
-}
-
-test
